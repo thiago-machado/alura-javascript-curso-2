2b269d9630d8c710dbc0a5d6f193089d70796e44
Thiago Machado <ti-thiago@outlook.com>
2021-02-17 17:17:54 -0300

    Interceptando a chamada de uma funcao

--- client/js/models/ListaNegociacoes.js
+++ client/js/models/ListaNegociacoes.js
@@ -1,26 +1,11 @@
 class ListaNegociacoes {
 
-  /*
-  Recebendo uma função como parâmetro.
-  Essa função exige receber uma instãncia de ListaNegociacoes como parâmetro.
-  Essa função irá executar a atualização da view NegociacoesView (acessar
-  NegociacaoController para ver isso).
-  */
-  constructor(armadilha) {
+  constructor() {
     this._negociacoes = []
-    this._armadilha = armadilha;
   }
 
   adiciona(negociacao) {
     this._negociacoes.push(negociacao);
-    /*
-    Toda vez que rodamos isso, estamos execuando o método que atualiza a view de negociação,
-    passando como parâmetro a própria instância de ListaNegociacoes.
-    Executar this._armadilha(this) seria como executar this._negociacoesView.update(modelo).
-    Vale ressaltar que o this em _negociacoesView pertence ao contexto de NegociacaoController e não
-    ao contexto de ListaNegociacoes. Por isso usamos arrow function para passar o método para esta classe.
-    */
-    this._armadilha(this);
   }
 
   get negociacoes() {
@@ -32,6 +17,5 @@ class ListaNegociacoes {
 
   esvazia() {
     this._negociacoes = [];
-    this._armadilha(this);
   }
 }
